7bf95cf94931c9bc97041d4e4dc642c3103f6da7
mrmitzh <zxzy12@hotmail.com>
2018-07-15 12:21:55 +0800

    update solution

--- tag_stack/  Premium New Playground mrmitzh  844. Backspace String Compare.cpp
+++ tag_stack/  Premium New Playground mrmitzh  844. Backspace String Compare.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     bool backspaceCompare(string S, string T) {
         std::stack<char> stackS;
@@ -16,5 +16,21 @@ public:
         check(S,stackS);
         check(T,stackT);
         return stackS == stackT;
+    }
+};*/
+
+class Solution
+{
+public:
+    bool backspaceCompare(string S, string T) {
+        for (int i = S.length() - 1, j = T.length() -1; ; i--, j--)
+        {
+            for(int b = 0; i >= 0 && (b || S[i] == '#'); i--)
+                b += S[i] == '#' ? 1 : -1;
+            for(int b = 0; j >= 0 && (b || T[j] == '#'); j--)
+                b += T[j] == '#' ? 1 : -1;
+            if(i<0||j<0||S[i]!=T[j]) // S[i] != T[j] exist for i>=0 and j>=0 with some word is not the same so we return false
+                return i == -1 && j == -1;
+        }
     }
 };

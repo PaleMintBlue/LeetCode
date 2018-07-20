43284546751b7a0691713172c9a99f0838f1c5e9
mrmitzh <zxzy12@hotmail.com>
2018-07-20 21:53:15 +0800

    update solution

---
+++ tag_linked_list/25. Reverse Nodes in k-Group.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* curr = head;
+        int count =  0;
+        while(curr&&count!=k)
+        {
+            curr = curr->next;
+            count++;
+        }
+        if(count==k)
+        {
+            curr = reverseKGroup(curr,k);
+            while(count-- > 0)
+            {
+                ListNode* tmp = head->next;
+                head->next = curr;
+                curr = head;
+                head = tmp;
+            }
+            head = curr;
+        }
+        return head;
+    }
+};

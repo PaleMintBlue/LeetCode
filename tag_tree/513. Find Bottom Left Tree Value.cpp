bea78660ce1a9391df924f26418abc23cb023751
mrmitzh <zxzy12@hotmail.com>
2018-07-24 00:31:34 +0800

    update solution

--- tag_tree/513. Find Bottom Left Tree Value.cpp
+++ tag_tree/513. Find Bottom Left Tree Value.cpp
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/*
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
@@ -22,5 +23,34 @@ public:
                 q.push(root->left);
         }
         return root->val;
+    }
+};*/
+class Solution {
+public:
+    int getHeight(TreeNode* root)
+    {
+        if(!root)
+            return 0;
+        return std::max(getHeight(root->left),getHeight(root->right))+1;
+    }
+    int findBottomLeftValue(TreeNode* root) {
+        if(!root)
+            return -1;
+        auto height = getHeight(root);
+        int ret = 0;
+        findMax(root,height,ret);
+        return ret;
+    }
+    void findMax(TreeNode* root,int height,int& ret)
+    {
+        if(!root)
+            return;
+        if(height==1)
+        {
+            ret = root->val;
+            return;
+        }
+        findMax(root->right,height-1,ret);
+        findMax(root->left,height-1,ret);
     }
 };

91a696220e69e41ae089bf9e4cf979f4eda01415
mrmitzh <zxzy12@hotmail.com>
2018-07-25 22:45:39 +0800

    update solution

---
+++ tag_tree/116. Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if(!root)
+            return;
+        TreeLinkNode* pre = root;
+        TreeLinkNode* cur = nullptr;
+        while(pre->left)
+        {
+            cur = pre;
+            while(cur)
+            {
+                cur->left->next = cur->right;
+                if(cur->next)
+                    cur->right->next = cur->next->left;
+                cur = cur->next;
+            }
+            pre = pre->left;
+        }
+        /*if(!root)
+            return;
+        if(root->left)
+        {
+            root->left->next = root->right;
+            if(root->next)
+                root->right->next = root->next->left;
+        }
+        connect(root->left);
+        connect(root->right);*/
+    }
+};

089776386fd114817991f45c6603ae7f0f832580
mrmitzh <zxzy12@hotmail.com>
2018-07-26 23:33:38 +0800

    update solution

--- tag_tree/450. Delete Node in a BST.cpp
+++ tag_tree/450. Delete Node in a BST.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(!root)
@@ -29,5 +29,48 @@ public:
         root->left = deleteNode(root->left,key);
         root->right = deleteNode(root->right,key);
         return root;
+    }
+};*/
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        TreeNode* cur = root;
+        TreeNode* pre = nullptr;
+        while(cur&&cur->val!=key)
+        {
+            pre = cur;
+            if(key<cur->val)
+                cur = cur->left;
+            else if(key>cur->val)
+                cur = cur->right;
+        }
+        if(!pre)
+            return deleteRootNode(cur);
+        if(pre->left==cur)
+            pre->left = deleteRootNode(cur);
+        else
+            pre->right = deleteRootNode(cur);
+        return root;
+    }
+private:
+    TreeNode* deleteRootNode(TreeNode* root)
+    {
+        if(!root)
+            return nullptr;
+        if(!root->left)
+            return root->right;
+        if(!root->right)
+            return root->left;
+        TreeNode* next = root->right;
+        TreeNode* pre = nullptr;
+        for(;next->left;pre = next,next = next->left)
+            ;
+        next->left = root->left;
+        if(root->right!=next)
+        {
+            pre->left = next->right;
+            next->right = root->right;
+        }
+        return next;
     }
 };

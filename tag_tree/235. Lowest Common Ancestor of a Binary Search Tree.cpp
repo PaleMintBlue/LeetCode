f0914cd274945e461e1808a457ac7dfe4e92b8f5
mrmitzh <zxzy12@hotmail.com>
2018-07-29 00:32:21 +0800

    update solution

--- tag_tree/235. Lowest Common Ancestor of a Binary Search Tree.cpp
+++ tag_tree/235. Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/*
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -15,5 +16,23 @@ public:
         if(p->val>root->val&&q->val>root->val)
             return lowestCommonAncestor(root->right,p,q);
         return root;
+    }
+};*/
+class Solution
+{
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root,TreeNode* p,TreeNode* q)
+    {
+        TreeNode* cur = root;
+        while(true)
+        {
+            if(p->val<cur->val&&q->val<cur->val)
+                cur = cur->left;
+            else if(p->val>cur->val&&q->val>cur->val)
+                cur = cur->right;
+            else
+                return cur;
+        }
+        return nullptr;
     }
 };

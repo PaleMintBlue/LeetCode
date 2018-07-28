6a97bb44abd3058315c9c4659ac602f14218e638
mrmitzh <zxzy12@hotmail.com>
2018-07-28 14:42:46 +0800

    update solution

--- tag_tree/107. Binary Tree Level Order Traversal II.cpp
+++ tag_tree/107. Binary Tree Level Order Traversal II.cpp
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/*
 class Solution {
 public:
     void levelOrderBottom(TreeNode* root,vector<vector<int>>& ret,int height)
@@ -26,6 +27,36 @@ public:
         vector<vector<int>> ret{};
         levelOrderBottom(root,ret,0);
         reverse(ret.begin(),ret.end());
+        return ret;
+    }
+};*/
+class Solution
+{
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root)
+    {
+        if(!root)
+            return {};
+        std::queue<TreeNode*> queue;
+        queue.push(root);
+        std::vector<std::vector<int>> ret;
+        while(!queue.empty())
+        {
+            std::vector<int> tmp;
+            int size = queue.size();
+            for(int i = 0; i < size;i++)
+            {
+                auto node = queue.front();
+                queue.pop();
+                tmp.push_back(node->val);
+                if(node->left)
+                    queue.push(node->left);
+                if(node->right)
+                    queue.push(node->right);
+            }
+            ret.push_back(tmp);
+        }
+        std::reverse(ret.begin(),ret.end());
         return ret;
     }
 };

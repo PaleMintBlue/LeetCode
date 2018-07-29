2736978349563c6e111c20b4a0c2124d2a08e512
mrmitzh <zxzy12@hotmail.com>
2018-07-29 14:47:19 +0800

    update solution

--- tag_tree/337. House Robber III.cpp
+++ tag_tree/337. House Robber III.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     int rob(TreeNode* root) {
         int withMe = 0, withoutMe = 0;
@@ -24,5 +24,37 @@ public:
         rob(root->right,withMeR,withoutMeR);
         withMe = root->val + withoutMeL + withoutMeR;
         withoutMe = max(withMeL,withoutMeL) + max(withMeR,withoutMeR);
+    }
+};*/
+class Solution
+{
+public:
+    int rob(TreeNode* root)
+    {
+        std::unordered_map<TreeNode*,int> map;
+        return robSub(root,map);
+    }
+private:
+    int robSub(TreeNode* root,std::unordered_map<TreeNode*,int>& map)
+    {
+        if(!root)
+            return 0;
+        if(map.count(root))
+            return map[root];
+        int val = 0;
+        if(root->left)
+        {
+            val += robSub(root->left->left,map) + robSub(root->left->right,map);
+        }
+        if(root->right)
+        {
+            val += robSub(root->right->left,map) + robSub(root->right->right,map);
+        }
+        
+        val = std::max(val + root->val,robSub(root->left,map)+robSub(root->right,map));
+        
+        map[root] = val;
+        
+        return val;
     }
 };

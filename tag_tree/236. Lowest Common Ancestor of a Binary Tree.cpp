61584bbe3b63d6d1dcadeb4fe12527f607142d7b
mrmitzh <zxzy12@hotmail.com>
2018-07-29 13:53:09 +0800

    update solution

--- tag_tree/236. Lowest Common Ancestor of a Binary Tree.cpp
+++ tag_tree/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/*
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -20,5 +21,42 @@ public:
             return right;
         else
             return left;
+    }
+};*/
+class Solution
+{
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p,TreeNode* q)
+    {
+        std::map<TreeNode*,TreeNode*> parent;
+        std::stack<TreeNode*> stack;
+        parent[root] = nullptr;
+        stack.push(root);
+        while(!parent.count(p)||!parent.count(q))
+        {
+            auto node = stack.top();
+            stack.pop();
+            if(node->left)
+            {
+                parent[node->left] = node;
+                stack.push(node->left);
+            }
+            if(node->right)
+            {
+                parent[node->right] = node;
+                stack.push(node->right);
+            }
+        }
+        std::set<TreeNode*> ancestors;
+        while(p)
+        {
+            ancestors.insert(p);
+            p = parent[p];
+        }
+        while(ancestors.find(q)==ancestors.end())
+        {
+            q = parent[q];
+        }
+        return q;
     }
 };

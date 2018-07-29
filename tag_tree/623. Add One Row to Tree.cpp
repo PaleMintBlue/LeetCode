2a52b3c72bacd0fa123700964e085cacca8f8392
mrmitzh <zxzy12@hotmail.com>
2018-07-29 14:54:15 +0800

    update solution

--- tag_tree/623. Add One Row to Tree.cpp
+++ tag_tree/623. Add One Row to Tree.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     TreeNode* addOneRow(TreeNode* root, int v, int d) {
         if(d==1)
@@ -35,5 +35,49 @@ public:
             return root;
         }
         return nullptr;
+    }
+};*/
+class Solution
+{
+public:
+    TreeNode* addOneRow(TreeNode* root,int v,int d)
+    {
+        if(!root)
+            return nullptr;
+        if(d==1)
+        {
+            auto newRoot = new TreeNode(v);
+            newRoot->left = root;
+            return newRoot;
+        }
+        std::queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            if(--d==0)
+                return root;
+            int n = q.size();
+            for(int i = 0; i < n; i++)
+            {
+                auto node = q.front();
+                q.pop();
+                if(d==1)
+                {
+                    auto left = new TreeNode(v);
+                    auto right = new TreeNode(v);
+                    left->left = node->left;
+                    right->right = node->right;
+                    node->left = left;
+                    node->right = right;
+                }else
+                {
+                    if(node->left)
+                        q.push(node->left);
+                    if(node->right)
+                        q.push(node->right);
+                }
+            }
+        }
+        return root;
     }
 };

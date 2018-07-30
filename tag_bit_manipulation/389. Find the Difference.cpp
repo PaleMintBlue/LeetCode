dbada7d14d93e702630150003e077e6ecf86fafb
mrmitzh <zxzy12@hotmail.com>
2018-07-30 23:20:24 +0800

    update solution

--- tag_bit_manipulation/389. Find the Difference.cpp
+++ tag_bit_manipulation/389. Find the Difference.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     char findTheDifference(string s, string t) {
         char c = 0;
@@ -7,5 +7,16 @@ public:
         for(const char& ch:t)
             c ^= ch;
         return c;
+    }
+};*/
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        std::unordered_map<char,int> map;
+        for(auto ch:s)
+            map[ch]++;
+        for(auto ch:t)
+            if(--map[ch]<0)
+                return ch;
     }
 };

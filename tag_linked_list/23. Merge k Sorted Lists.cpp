fb7fc445d3f25ffdf56df1db20d4be883010f765
mrmitzh <30907704+mrmitzh@users.noreply.github.com>
2018-08-11 12:56:19 +0800

    update solution

--- tag_linked_list/23. Merge k Sorted Lists.cpp
+++ tag_linked_list/23. Merge k Sorted Lists.cpp
@@ -6,6 +6,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+/*
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
@@ -36,4 +37,47 @@ public:
             return l2;
         }
     }
+};*/
+class Solution
+{
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists)
+    {
+        if(lists.empty())
+            return nullptr;
+        std::priority_queue<ListNode*,std::vector<ListNode*>,cmp> q;
+        int n = lists.size();
+        for(int i = 0; i < n; i++)
+        {
+            ListNode* head = lists[i];
+            while(head)
+            {
+                q.push(head);
+                head = head->next;
+            }
+        }
+        if(q.empty())
+            return nullptr;
+        ListNode* head = q.top();
+        q.pop();
+        ListNode dummy;
+        dummy.next = head;
+        while(!q.empty())
+        {
+            head->next = q.top();
+            head = head->next;
+            q.pop();
+        }
+        head->next = nullptr;
+        head = dummy.next;
+        return head;
+    }
+class cmp
+{
+public:
+        bool operator() (ListNode* l1,ListNode* l2)
+        {
+            return l1->val > l2->val;
+        }
+};
 };

7bc01a4307c4ab3738b6a1ab1ffdf6dbedb24f44
mrmitzh <30907704+mrmitzh@users.noreply.github.com>
2018-08-16 11:43:52 +0800

    update solution

--- tag_hash_table/524. Longest Word in Dictionary through Deleting.cpp
+++ tag_hash_table/524. Longest Word in Dictionary through Deleting.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     string findLongestWord(string s, vector<string>& d) {
         std::sort(d.begin(),d.end(),[](const std::string& a,const std::string& b)
@@ -15,5 +15,24 @@ public:
                 return str;
         }
         return "";
+    }
+};*/
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& d) {
+        std::string ret;
+        for(const auto& str:d)
+        {
+            int i = 0;
+            for(auto ch:s)
+            {
+                if(i < str.size() && ch == str[i])
+                    i++;
+            }
+            if(i==str.size() && str.size() >= ret.size())
+                if(str.size() > ret.size() || str < ret)
+                    ret = str;
+        }
+        return ret;
     }
 };

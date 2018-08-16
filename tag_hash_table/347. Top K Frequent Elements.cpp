2afda07f252fda0592deda12a8f016de24d5d6c8
mrmitzh <30907704+mrmitzh@users.noreply.github.com>
2018-08-16 11:24:11 +0800

    update solution

--- tag_hash_table/347. Top K Frequent Elements.cpp
+++ tag_hash_table/347. Top K Frequent Elements.cpp
@@ -19,6 +19,7 @@ public:
         return ret;
     }
 };*/
+/*
 class Solution
 {
 public:
@@ -34,6 +35,40 @@ public:
                          {
                              return map[l] > map[r];
                          });
+        ret.erase(ret.begin()+k,ret.end());
+        return ret;
+    }
+};*/
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int>& nums,int k)
+    {
+        std::vector<int> ret;
+        std::unordered_map<int,int> map;
+        for(const auto& val:nums)
+            map[val]++;
+        for(const auto& pair:map)
+            ret.push_back(pair.first);
+        std::function<void (int,int)> quick_select = [&map,&ret,k,&quick_select] (int lo,int hi) 
+        {
+            int pivot_freq = map[ret[hi-1]];
+            int idx = lo;
+            
+            for(int i = lo;i < hi; i++)
+            {
+                if(map[ret[i]] >= pivot_freq)
+                    std::swap(ret[i],ret[idx++]);
+            }
+            
+            if(idx==k)
+                return;
+            else if(idx < k )
+                quick_select(idx,hi);
+            else
+                quick_select(lo,idx-1);
+        };
+        quick_select(0,ret.size());
         ret.erase(ret.begin()+k,ret.end());
         return ret;
     }

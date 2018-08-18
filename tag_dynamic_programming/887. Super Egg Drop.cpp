3bd9edb69777a73a518bb77544651f7b1488c252
mrmitzh <zxzy12@hotmail.com>
2018-08-18 15:59:51 +0800

    update solution

--- tag_dynamic_programming/887. Super Egg Drop.cpp
+++ tag_dynamic_programming/887. Super Egg Drop.cpp
@@ -1,6 +1,6 @@
-class Solution {
+/*class Solution {
 public:
-    /*int superEggDrop(int K, int N) {
+    int superEggDrop(int K, int N) {
         std::vector<std::vector<int>> dp(N+1,std::vector<int>(K+1,0));
         int m = 0;
         while(dp[m][K]<N)
@@ -10,20 +10,86 @@ public:
                 dp[m][k] = dp[m-1][k-1] + dp[m-1][k] + 1;
         }
         return m;
-    }*/
+    }
+};*/
+/*
+class Solution
+{
+public:
     int superEggDrop(int K, int N) {
-        /*
-        * dp[m][k] = dp[m-1][k-1] + dp[m-1][k] + 1 
-        * since we only use dp[m-1][k] once each time we can change the equation to 
-        * dp[m-1][k] = dp[m-1][k-1] + dp[m-1][k] + 1 which is 
-        * dp[m-1][k] += dp[m-1][k-1] + 1
-        * since m-1 is of no use --> dp[k] += dp[k-1] + 1 
-        */
+        // dp[m][k] = dp[m-1][k-1] + dp[m-1][k] + 1 
+        // since we only use dp[m-1][k] once each time we can change the equation to 
+        // dp[m-1][k] = dp[m-1][k-1] + dp[m-1][k] + 1 which is 
+        // dp[m-1][k] += dp[m-1][k-1] + 1
+        // since m-1 is of no use --> dp[k] += dp[k-1] + 1 
         vector<int> dp(K + 1, 0);
         int m;
         for (m = 0; dp[K] < N; m++)
             for (int k = K; k > 0; --k)
                 dp[k] += dp[k - 1] + 1;
         return m;
+    }
+};*/
+/*
+class Solution
+{
+public:
+    int superEggDrop(int K, int N) {
+        std::vector<std::vector<int>> dp(K+1,std::vector<int>(N+1));
+        return helper(K,N,dp);
+    }
+private:
+    int helper(int K,int N,std::vector<std::vector<int>>& dp)
+    {
+        if(N<=1)
+            return N;
+        if(K==1)
+            return N;
+        if(dp[K][N]>0)
+            return dp[K][N];
+        int min = N;
+        for(int i = 1; i <= N;i++)
+        {
+            int left = helper(K-1,i-1,dp);
+            int right = helper(K,N-i,dp);
+            min = std::min(min,std::max(left,right)) + 1;
+        }
+        dp[K][N] = min;
+        return min;
+    }
+};*/
+class Solution
+{
+public:
+    int superEggDrop(int K, int N) {
+        std::vector<std::vector<int>> dp(K+1,std::vector<int>(N+1));
+        return helper(K,N,dp);
+    }
+private:
+    int helper(int K,int N,std::vector<std::vector<int>>& dp)
+    {
+        if(N<=1)
+            return N;
+        if(K==1)
+            return N;
+        if(dp[K][N]>0)
+            return dp[K][N];
+        int result = N;
+        int low = 1, high = N;
+        while(low < high)
+        {
+            int mid = low+(high-low)/2;
+            int left = helper(K-1,mid-1,dp);
+            int right = helper(K,N-mid,dp);
+            result = std::min(result,std::max(left,right)) + 1;
+            if(left==right)
+                break;
+            else if(left < right)
+                low = mid+1;
+            else
+                high = mid;
+        }
+        dp[K][N] = result;
+        return result;
     }
 };

1fec083302b7054609ad811b45ce2199c1c9ebb4
mrmitzh <zxzy12@hotmail.com>
2018-08-20 21:48:31 +0800

    update solution

--- tag_array/768. Max Chunks To Make Sorted II.cpp
+++ tag_array/768. Max Chunks To Make Sorted II.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         std::vector<int> sorted(arr.begin(),arr.end());
@@ -14,5 +14,34 @@ public:
                 ret++;
         }
         return ret;
+    }
+};*/
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        std::vector<int> sorted(arr.begin(),arr.end());
+        std::sort(sorted.begin(),sorted.end());
+        std::vector<int> duplicate_arr(arr.size(),0);
+        for(int i = arr.size()-2; i>=0 ;i--)
+        {
+            if(sorted[i] == sorted[i+1])
+                duplicate_arr[i] = duplicate_arr[i+1]+1;
+        }
+        int min = std::numeric_limits<int>::max();
+        int counter = 0;
+        int min_counter = 0;
+        for(int i = arr.size()-1; i >= 0 ; i--)
+        {
+            if(arr[i]==min)
+                min_counter++;
+            else if(arr[i]<min)
+            {
+                min_counter = 0;
+                min = arr[i];
+            }
+            if(min==sorted[i]&&min_counter==duplicate_arr[i])
+                counter++;
+        }
+        return counter;
     }
 };

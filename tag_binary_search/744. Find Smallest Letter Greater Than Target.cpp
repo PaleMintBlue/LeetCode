7bd943229f1a391e37d1cd95401951a9d2a12d11
mrmitzh <zxzy12@hotmail.com>
2018-08-21 00:48:01 +0800

    update solution

--- tag_binary_search/744. Find Smallest Letter Greater Than Target.cpp
+++ tag_binary_search/744. Find Smallest Letter Greater Than Target.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
         auto upper = std::upper_bound(letters.begin(),letters.end(),target);
@@ -6,5 +6,23 @@ public:
             return letters[0];
         else
             return *upper;
+    }
+};*/
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        if(target>=letters.back())
+            return letters[0];
+        int left = 0;
+        int right = letters.size()-1;
+        while(left < right)
+        {
+            int mid = left + (right-left)/2;
+            if(letters[mid]<=target)
+                left = mid+1;
+            else
+                right = mid;
+        }
+        return letters[right];
     }
 };

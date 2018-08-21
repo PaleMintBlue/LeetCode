724cbaefc6ce5d4f2a771cca9170238919ca835f
mrmitzh <zxzy12@hotmail.com>
2018-08-21 16:46:07 +0800

    update solution

--- tag_hash_table/720. Longest Word in Dictionary.cpp
+++ tag_hash_table/720. Longest Word in Dictionary.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     string longestWord(vector<string>& words) {
         std::sort(words.begin(),words.end());
@@ -14,4 +14,57 @@ public:
         }
         return res;
     }
+};*/
+struct TrieNode
+{
+    bool isWord = false;
+    TrieNode* children[26] = {nullptr};
+};
+class Solution {
+public:
+    Solution()
+    {
+        root = new TrieNode();
+        root->isWord = true;
+    }
+    bool canForm(TrieNode* root,const std::string& word)
+    {
+        for(auto ch:word)
+        {
+            if(root->children[ch-'a']==nullptr||!root->isWord)
+                return false;
+            root = root->children[ch-'a'];
+        }
+        return root->isWord;
+    }
+    string longestWord(vector<string>& words) {
+        for(const auto& str:words)
+        {
+            TrieNode* temp = root;
+            for(auto ch:str)
+            {
+                if(temp->children[ch-'a']==nullptr)
+                    temp->children[ch-'a'] = new TrieNode();
+                temp = temp->children[ch-'a'];
+            }
+            temp->isWord = true;
+        }
+        std::string res;
+        for(const auto& str:words)
+        {
+            if(str.size()>=res.size()&&canForm(root,str))
+            {
+                if(str.size()>res.size())
+                    res = str;
+                else
+                {
+                    if(str < res)
+                        res = str;
+                }
+            }
+        }
+        return res;
+    }
+private:
+    TrieNode* root = nullptr;
 };
